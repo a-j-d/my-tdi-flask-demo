d9ef3c9796983700cf7818354e454d2cfcfec9d9
a-j-d <amrejd.fun@gmail.com>
2017-06-14 20:04:31 +0000

    added some templates

--- app.py
+++ app.py
@@ -1,18 +1,29 @@
 import os
 from flask import Flask, render_template, request, redirect
+import requests
 
 app = Flask(__name__)
 
+app.vars={}
+
 @app.route('/')
 def main():
-  return redirect('/index')
+    return redirect('/index')
 
 @app.route('/index')
-def index():
-  #return render_template('index.html')
-  return render_template('home_page.html')
+def index_get():
+    if request.method == 'GET':
+        return render_template('index.html')
+
+@app.route('/index')
+def index_post():
+    if request.method == 'POST':
+        #app.vars['stock_name'] = request.form('ticker')
+        app.vars['stock_name'] = 'dummy_ticker_name'
+        
+    return render_template('plot_page.html',tckname=app.vars['stock_name'])
 
 if __name__ == '__main__':
-  # app.run(host='0.0.0.0',port=33507)
-  port = int(os.environ.get("PORT", 5000))
-  app.run(host='0.0.0.0',port=port)
+    # app.run(host='0.0.0.0',port=33507)
+    port = int(os.environ.get("PORT", 5000))
+    app.run(host='0.0.0.0',port=33057)
