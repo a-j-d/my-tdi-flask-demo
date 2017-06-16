14cfe9a803f50b85fd0f54e04a6816637497f2ac
a-j-d <amrejd.fun@gmail.com>
2017-06-16 02:11:18 +0000

    updating app

--- app.py
+++ app.py
@@ -43,23 +43,30 @@ def index_post():
 
 @app.route('/plot_data', methods=['GET','POST'])
 def plot_data():
+    
     print '/plot data was accessed' ; sys.stdout.flush();
+
     if request.method == 'GET':
         print 'plot data method was GET' ; sys.stdout.flush();
+        
         # prepare api request url
         today = dt.date.today().isoformat()
         from_date = (today - dt.timedelta(days=31)).isoformat()
         print 'dates are: ', today, from_date; sys.stdout.flush();
+        
         request_args = {'column_index':'4', 'start_date':from_date, 'end_date':today, 'frequency':'daily', 'api_key':'8nDck7hx1ivMZH1LpRKg'}
         
         print 'request_args is: ', request_args; sys.stdout.flush();
         
-        req = requests.get("https://www.quandl.com/api/v3/datasets/WIKI/" + app.vars['stock_name']+".json",data=request_args)
+        req = requests.get("https://www.quandl.com/api/v3/datasets/WIKI/" + app.vars['stock_name']+".json",data=request_args)    
         
+        print 'Was request successful? ', req.status_code == req.status_code
         
         if req.status_code == requests.code.ok:
             # ind1 = open, ind4 = close, ind8 = ajd. open, ind11 = adj. close
+            
             print( 'displaying plot..., plot_data request was get' ); sys.stdout.flush();
+            """
 
             payload={'column_index':'4','end_date': dt.date.today(), 'frequency': 'daily', 'api_key':'8nDck7hx1ivMZH1LpRKg','start_date': dt.date.today() - dt.timedelta(days=31)}
 
@@ -86,6 +93,8 @@ def plot_data():
 
             html = render_template('embed.html', plot_script=script, plot_div=div, js_resources=js_resources, css_resources=css_resources, color=color, _from=from_date, to=today)#, tckname=app.vars['stock_name'],pricestring=", ".join(app.vars['features']))
             return encode_utf8(html)
+        """
+            return render_template('plot_page.html')
        
         # display error below otherwise
         else:
@@ -93,7 +102,7 @@ def plot_data():
             
     elif request.method == 'POST':
         #
-        print ('request was post');sys.stdout.flush();
+        print ('plot data request was post');sys.stdout.flush();
         return redirect('/index')
     else: 
         print ' /plot_data request was neither GET nor POST ';sys.stdout.flush();
