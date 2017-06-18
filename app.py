406f09f9cb954a74e11b5be7bf8d04ace3bd0dc0
a-j-d <amrejd.fun@gmail.com>
2017-06-18 04:26:22 +0000

    final

--- app.py
+++ app.py
@@ -59,9 +59,6 @@ def plot_data():
         from_date = (dt.date.today() - dt.timedelta(days=31)).isoformat()
         print 'dates are: ', today, from_date; sys.stdout.flush();
         
-        ###
-        #request_args = {'column_index':'4', 'exclude_column_names':'true', 'start_date':from_date, 'end_date':today, 'frequency':'daily', 'api_key':'8nDck7hx1ivMZH1LpRKg'}
-        ###
         request_args = {'start_date':from_date, 'end_date':today, 'frequency':'daily', 'api_key':'8nDck7hx1ivMZH1LpRKg'}
         
         print 'request_args is: ', request_args; sys.stdout.flush();
@@ -76,85 +73,28 @@ def plot_data():
             
             print( 'displaying plot..., plot_data request was get' ); sys.stdout.flush();
             
-            ###
             df = pd.DataFrame(req.json())
-            ###
-            print "df ", df; sys.stdout.flush(); 
-            
-            ###
+
             dat = pd.DataFrame(req.json()['dataset']['data'])
             
-            ###
-            print 'dat' , dat; sys.stdout.flush();
-            print 'dat_col_names' , df['dataset']['column_names']; sys.stdout.flush();
-            
-            ###
             dat_col_ind = [df['dataset']['column_names'].index(elem) for elem in app.vars['checkboxes']]
-            print 'dat_col_ind ', dat_col_ind ; sys.stdout.flush();
             
             colors = base_colors[0:len(dat_col_ind)]
 
-            ###
-            print "dat info", type(dat), dat; sys.stdout.flush();
-            
-            ###
             indtoplot = []
-            ###
+
             dattoplot = []
             
-            ###
-            print "dat[0] ", dat[0]
-            ###
-            print "dat[4] ", dat[4]
-            ###
-            print "dat[1] ", dat[1]
-            ###
-            print "dat[8] ", dat[8]
-            
-
-            ###
             for index in dat_col_ind:
                 indtoplot.append(list(pd.to_datetime(dat[0]).values))
-                ###
-                ##indtoplot.append(list(range(len(dat[1].values))))
-                ###
                 dattoplot.append(list(dat[index].values))
             
-            ###
-            print "indtoplot ", indtoplot; sys.stdout.flush();
-            ###
-            print "dattoplot ", dattoplot; sys.stdout.flush();
-            
-            ###
-            #df = pd.DataFrame(req.json())
-            ###
-            #print "df: ", df; sys.stdout.flush();
-            
-            ###
-            #datdic = dict(req.json()['dataset']['data'])
-            ###
-            #print "datdic: ", datdic; sys.stdout.flush();
-            
-            ###
-            #ordatdic = collections.OrderedDict(sorted(datdic.items()))
-            ###
-            #print "ordatdic: ", ordatdic ; sys.stdout.flush();
-            
-            ###
-            #df2 = pd.DataFrame(data=ordatdic.values(), index=pd.DatetimeIndex(ordatdic.keys()), columns= ['Close'])
-            ###
-            #print "df2 data is: ", df2['Close']; sys.stdout.flush();
-            ###
-            #print "df2 index is: ", df2.index
             
             fig = figure(width=500, height=300)#, x_axis_type="datetime") 
             for (xs, ys, color, leg) in zip(indtoplot, dattoplot, colors, app.vars['checkboxes']):
                 fig.line(xs, ys, color=color, legend=leg)
             ###
             #fig.multi_line(indtoplot, dattoplot, color=colors)
-            #fig.line(df2.index,df2['Close'])#,color="#2222aa",line_width=5)
-            #fig.line(df2.index,df2['Close'])#,color="#2222aa",line_width=5)
-            ###
             fig.xaxis.formatter = DatetimeTickFormatter(days=["%d-%m-%y"], months=["%d-%m-%y"], years=["%d-%m-%y"])
             fig.title.text="Quandl WIKI data for '"+app.vars['stock_name']+"'"
             fig.legend.location="top_left"
@@ -166,10 +106,8 @@ def plot_data():
             
             script, div = components(fig)
             
-            #html = render_template('embed.html', plot_script=script, plot_div=div, js_resources=js_resources, css_resources=css_resources, color=color, _from=from_date, to=today)#, tckname=app.vars['stock_name'],pricestring=", ".join(app.vars['checkboxes']))
             html = render_template('plot_page.html', plot_script=script, plot_div=div, js_resources=js_resources, css_resources=css_resources)#, tckname=app.vars['stock_name'],pricestring=", ".join(app.vars['checkboxes']))
             return encode_utf8(html)
-            #return render_template('plot_page.html')
             
             # display error below otherwise
         else:
