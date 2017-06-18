32f6b1dc7dec73b4f45a64b7210d2c76d1fde426
a-j-d <amrejd.fun@gmail.com>
2017-06-18 03:46:32 +0000

    cleaning up files and text

--- push_to_heroku.sh
+++ push_to_heroku.sh
@@ -1,9 +1,10 @@
 git add --all
 echo "shell executed: git add --all"
-#echo "enter commit label: "
-#read inp
-#git commit -m  "$inp"
-git commit -m "updating app"
-echo 'shell executed: git commit -m "updating app"' 
+echo "enter commit label: "
+read inp
+git commit -m  "$inp"
+echo 'shell executed: git commit -m "$inp"' 
+#git commit -m "updating app"
+#echo 'shell executed: git commit -m "updating app"' 
 echo "shell executing...: git push heroku master"
 git push heroku master
